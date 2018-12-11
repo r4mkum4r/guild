ad742d8128cab87d28372bc00e1bc92cda082b2d
raramakrishna <raramakrishna@egencia.com>
2018-12-11 20:36:07 +0530

    WIP

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
+// import Vue from 'vue';
 import AssignArrangers from './components/vue/assign-arrangers.js';
 
-const MODAL_TMPL = Handlebars.templates['partials/common/components/user-picker']; 
+const MODAL_TMPL = Handlebars.templates['partials/common/components/user-picker'];
 
 const MODAL_OPTIONS = {
   modalId: 'user-picker',
@@ -20,7 +21,7 @@ uitk.subscribe('modal.appended', onModalOpen);
 
 // assignApprovers();
 
-function assignApprovers () {
+function assignApprovers() {
   let modal;
 
   modal = uitk.modal.create(MODAL_OPTIONS);
@@ -28,14 +29,13 @@ function assignApprovers () {
   modal.open();
 }
 
-function onModalOpen () {
-  console.log('modal: open');
-  
+function onModalOpen() {
+
+  httpVueLoader.register(Vue, '/static/components/vue/hello-world.vue');
+
   new Vue({
-    el: '#user-picker-content',
-    components: {AssignArrangers}
+    el: '#user-picker-content'
   });
-  
 }
 
 // new Vue({
